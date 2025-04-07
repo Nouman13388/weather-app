ad3533785fccf9ae24ac86a455e3b8c5baa468b2
MUHAMMAD NOUMAN <nouman.nomi1338@gmail.com>
2025-04-07 17:48:22 +0500

    async
    async is implemented to load data of cities from a JSON file

--- src/SearchContainer.jsx
+++ src/SearchContainer.jsx
@@ -1,53 +1,56 @@
 import React, { useState, useEffect } from "react";
 
 function SearchContainer({ setCityToSearch }) {
-    const [cities, setCities] = useState([]);
-    const [selectedCity, setSelectedCity] = useState("");
+  const [cities, setCities] = useState([]);
+  const [selectedCity, setSelectedCity] = useState("");
 
-    useEffect(() => {
-        fetch("src/cities.json")
-            .then((response) => response.json())
-            .then((data) => {
-                setCities(data.cities);
-            })
-            .catch((error) => {
-                console.error("Error loading city data:", error);
-            });
-    }, []);
-
-    const handleChange = (e) => {
-        setSelectedCity(e.target.value);
+  const loadData = async () => {
+    try {
+      const response = await fetch("src/cities.json");
+      const loadedData = await response.json();
+      setCities(loadedData.cities);
+    } catch (error) {
+      console.error("Error loading Local Data: ", error);
     }
+  };
 
-    const handleClick = () => {
-        setCityToSearch(selectedCity);
-    }
+  const handleChange = (e) => {
+    setSelectedCity(e.target.value);
+  };
 
-    return (
-        <section className="search-container">
-            <datalist id="suggestions">
-                {cities.map((city, index) => (
-                    <option key={index} value={city} />
-                ))}
-            </datalist>
-            <input
-                type="text"
-                id="search-bar"
-                className="search-bar"
-                list="suggestions"
-                placeholder="City Name..."
-                value={selectedCity}
-                onChange={handleChange}
-            />
-            <button
-                className="search-button"
-                id="search-button"
-                onClick={handleClick}
-            >
-                Search
-            </button>
-        </section>
-    );
+  const handleClick = () => {
+    setCityToSearch(selectedCity);
+  };
+
+  useEffect(() => {
+    loadData;
+  }, []);
+
+  return (
+    <section className="search-container">
+      <datalist id="suggestions">
+        {cities.map((city, index) => (
+          <option key={index} value={city} />
+        ))}
+      </datalist>
+      <input
+        type="text"
+        id="search-bar"
+        className="search-bar"
+        list="suggestions"
+        placeholder="City Name..."
+        value={selectedCity}
+        onChange={handleChange}
+      />
+      <button
+        className="search-button"
+        id="search-button"
+        onClick={handleClick}
+      >
+        Search
+      </button>
+    </section>
+  );
 }
 
 export default SearchContainer;
