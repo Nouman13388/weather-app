fb4b49fa8a4a8cc2b8bdee428283b459b4426f3c
MUHAMMAD NOUMAN <nouman.nomi1338@gmail.com>
2025-04-16 10:33:55 +0500

    path name added
    i've changed the path name of the page according to navigation of the page

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,39 @@
-import { useState } from "react";
 import "./App.css";
 import Home from "./Home";
 import Settings from "./Settings";
+import { useState, useEffect } from "react";
 
 function App() {
+    const [isPressed, setIsPressed] = useState(false);
 
-  const [isPressed, setIsPressed] = useState(false);
+    useEffect(() => {
+        if (isPressed) {
+            window.history.pushState({}, '', '/settings');
+        } else {
+            window.history.pushState({}, '', '/');
+        }
+        console.log("pathName: ",window.location.pathname);
+    }, [isPressed]);
 
-  const handleSettings = () => {
-    setIsPressed(true)
-    console.log("Settings Button is Pressed");
-  }
+    const handleSettings = () => {
+        setIsPressed(true);
+        console.log("Settings Icon is Pressed");
+    };
 
-  const handleBack = () => {
-    setIsPressed(false)
-    console.log("Back Button is Pressed");
-  }
+    const handleBack = () => {
+        setIsPressed(false);
+        console.log("Back Button is Pressed");
+    };
 
-  const renderPages = () => {
-    if(!isPressed)
-      return <Home renderSettings={handleSettings}/>  
-    else
-      return <Settings renderHome={handleBack}/>
-  }
+    const renderPages = () => {
+        if (!isPressed) {
+            return <Home renderSettings={handleSettings} />;
+        } else {
+            return <Settings renderHome={handleBack} />;
+        }
+    };
 
-  return (
-    renderPages()
-  );
+    return renderPages();
 }
 
 export default App;
